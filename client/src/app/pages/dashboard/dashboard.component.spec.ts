29acbc5625b283ecf1e2854cbefb7875b7a45c1d
giovanni <giovannipuppets@gmail.com>
2025-09-25 09:16:15 -0300

    test: corrige imports do Angular Material nos testes e ajusta teste do título do AppComponent

--- client/src/app/pages/dashboard/dashboard.component.spec.ts
+++ client/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -3,6 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -11,7 +17,16 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
-  imports: [HttpClientTestingModule, FormsModule]
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatInputModule,
+        MatCardModule,
+        MatIconModule,
+        MatListModule
+      ]
     })
     .compileComponents();
     fixture = TestBed.createComponent(DashboardComponent);
